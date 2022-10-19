922d28b6122819043db7215744da279f4e1ba1be
Bukkysax <bukolaowoeye19@gmail.com>
2022-10-19 14:58:17 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,13 +5,13 @@
  */
 void jack_bauer(void)
 {
-	int hour, minutes;
+	int hour, minute;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putcha(r(hour / 10) + '0');
+			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar('-');
 			_putchar((minute / 10) + '0');
